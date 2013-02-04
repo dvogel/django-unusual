3efb12fb89969ecd9b87d35fb45043938fea2c23
Drew Vogel <dvogel@sunlightfoundation.com>
2013-02-04 15:54:23 -0500

    Initial version

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-unusual",
+    version='0.1',
+    packages=find_packages(),
+    description="Allows django apps to raise HttpResponse objects as exceptions.",
+    author="Drew Vogel",
+    author_email="drewpvogel@gmail.com",
+    license='BSD',
+    platforms=["any"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=['django']
+)
